aee8a6a1f2f6600980193762b706dcdf33cf0839
Drew Crawford <drew@sealedabstract.com>
2022-07-03 15:31:00 -0500

    [nsstring] from_borrowed_os_str

--- src/nsstring.rs
+++ src/nsstring.rs
@@ -1,3 +1,5 @@
+use std::ffi::OsStr;
+use std::path::Path;
 use objr::bindings::*;
 use crate::NSUInteger;
 
@@ -10,9 +12,6 @@ objc_selector_group! {
 }
 
 pub trait NSStringExtension {
-    fn from_borrowed_str<'a>(str: &'a str, pool: &ActiveAutoreleasePool) -> StrongLifetimeCell<'a, NSString>;
-}
-impl NSStringExtension for NSString {
     ///This function creates a string that borrows the argument.
     ///
     ///In Rust, the string is valid for the lifetime of the argument.  'By convention', most objc code
@@ -20,6 +19,19 @@ impl NSStringExtension for NSString {
     /// with extended lifetime, although this is not guaranteed.
     ///
     /// I'm marking this API safe because, calling such functions are unsafe anyway, although you should be aware of the additional risk.
+    fn from_borrowed_str<'a>(str: &'a str, pool: &ActiveAutoreleasePool) -> StrongLifetimeCell<'a, NSString>;
+    /**
+
+    Creates a string that borrows the argument.
+
+    In Rust, the string is valid for the lifetime of the argument.  'By convention', most objc cod
+    that encounters the NSString will end up copying it, which will copy the data into a new objec
+    with extended lifetime, although this is not guaranteed
+      */
+    fn from_borrowed_os_str<'a>(str: &'a OsStr, pool: &ActiveAutoreleasePool) -> StrongLifetimeCell<'a, NSString>;
+}
+impl NSStringExtension for NSString {
+
     fn from_borrowed_str<'a>(str: &'a str, pool: &ActiveAutoreleasePool) -> StrongLifetimeCell<'a,NSString> {
         let bytes = str.as_bytes().as_ptr();
         let len = str.len() as NSUInteger;
@@ -32,6 +44,23 @@ impl NSStringExtension for NSString {
             Self::assume_nonnil(raw).assume_retained_limited()
         }
     }
+
+    fn from_borrowed_os_str<'a>(str: &'a OsStr, pool: &ActiveAutoreleasePool) -> StrongLifetimeCell<'a, NSString> {
+        use std::os::unix::ffi::OsStrExt;
+        let bytes = str.as_bytes();
+        let len = bytes.len() as NSUInteger;
+
+        let encoding: NSUInteger = 4;
+        unsafe {
+            let alloc = Self::class().alloc(pool);
+
+            let raw = Self::perform(alloc, Sel::initWithBytesNoCopy_length_encoding_freeWhenDone(), &pool, (bytes.as_ptr().assume_nonmut_perform(), len,encoding,false ));
+            //we assume this will work since it's str already
+            Self::assume_nonnil(raw).assume_retained_limited()
+        }
+
+    }
+
 }
 
 
@@ -49,4 +78,14 @@ impl NSStringExtension for NSString {
     let new_str = "test 456 456".to_owned();
     println!("{}",new_str);
 
+}
+
+#[test] fn os_str() {
+    let path = Path::new("test my på†h");
+    let os_str = path.as_os_str();
+    autoreleasepool(|pool| {
+        let nsstring = NSString::from_borrowed_os_str(os_str,pool);
+        let description = nsstring.description(pool).to_string();
+        assert_eq!(&description, "test my på†h")
+    });
 }
